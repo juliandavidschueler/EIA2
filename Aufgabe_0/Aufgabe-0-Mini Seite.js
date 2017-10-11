ded7e0668c74f44f48b8521ec89049d9ca93ed32
julian <julian@julians-mbp.eduroam.hs-furtwangen.de>
2017-10-11 17:05:46 +0200

    Aufgabe 0

--- Aufgabe_0/Aufgabe-0-Mini Seite.js
+++ Aufgabe_0/Aufgabe-0-Mini Seite.js
@@ -1,11 +1,11 @@
-var name = prompt("Julian");
+var name = prompt("Bitte gib deinen Namen ein");
 function init() {
     let node = document.getElementById("Inhalt");
     if (name.length < 1) {
         node.innerHTML = "Gib gefälligst deinen Namen ein!";
     }
     if (name.length >= 1) {
-        node.innerHTML = "Klasse" + "Julian";
+        node.innerHTML = "Servus" + " " + name;
     }
 }
 window.addEventListener("load", init);
--- Aufgabe_0/Aufgabe-0-Mini Seite.ts
+++ Aufgabe_0/Aufgabe-0-Mini Seite.ts
@@ -1,12 +1,12 @@
 
-var name:string = prompt ("Julian");
+var name:string = prompt ("Bitte gib deinen Namen ein");
 
 function init() : void {
     let node = document.getElementById("Inhalt");
     
     if(name.length < 1) { node.innerHTML = "Gib gefälligst deinen Namen ein!"; }
     
-    if(name.length >= 1) { node.innerHTML = "Klasse" + "Julian"; }
+    if(name.length >= 1) { node.innerHTML = "Servus" + " " + name; }
     
     }
 
