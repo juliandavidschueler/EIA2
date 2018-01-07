bba0292116a44a260c62d341dfc49158c3ed5e10
julian <julian@julians-mbp.fritz.box>
2018-01-07 16:11:25 +0100

    Update

--- Aufgabe_09/Aufgabe-09-DynHTML.js
+++ Aufgabe_09/Aufgabe-09-DynHTML.js
@@ -1,38 +1,84 @@
 /*
-Aufgabe: (8 DynHTML)
+Aufgabe: (9 DynHTML)
 Name: (Julian Schüler)
 Matrikel: (256033)
-Datum: (07.12.2017)
+Datum: (15.12.2017)
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 var Aufgabe09;
 (function (Aufgabe09) {
-    var L08_Events;
-    (function (L08_Events) {
-        window.addEventListener("load", init);
-        document.addEventListener("keydown", handleKeydown);
-        //document.onkeydown = handleKeydown;
-        document.addEventListener("click", handleClick);
-        function handleClick(_event) {
-            console.log("Target: " + _event.target);
-            console.log("currentTarget: " + _event.currentTarget);
+    window.addEventListener("load", Erpresserbrief);
+    window.addEventListener("keydown", handleKeyDown);
+    let text;
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let saveLetter = "";
+    function Erpresserbrief() {
+        for (let i = 0; i < letters.length; i++) {
+            let l = document.createElement("div");
+            l.style.width = "35px";
+            l.style.height = "35px";
+            l.style.margin = "5px";
+            l.style.backgroundColor = "#b7b7b7";
+            l.style.color = "white";
+            l.style.fontSize = "25px";
+            l.innerText = letters[i];
+            l.id = letters[i];
+            l.className = "letters";
+            l.addEventListener("click", handleLetterClick);
+            document.body.appendChild(l);
         }
-        function handleKeydown(_event) {
-            console.log(_event);
+        let box = document.createElement("div");
+        box.style.width = "1165px";
+        box.style.height = "500px";
+        box.style.marginTop = "20px";
+        box.style.backgroundColor = "#b7b7b7";
+        box.addEventListener("click", handleBoxClick);
+        document.body.appendChild(box);
+    }
+    function handleLetterClick(_event) {
+        console.log(_event.target);
+        let l = _event.target;
+        l.style.color = "#ffffff";
+        l.style.backgroundColor = "#000000";
+        saveLetter = l.id;
+        let letterList = document.getElementsByClassName("letters"); //Jedes Div der Klasse letters in Variable speichern
+        for (let i = 0; i < letterList.length; i++) {
+            if (saveLetter != letterList[i].id) {
+                letterList[i].style.backgroundColor = "#b7b7b7";
+            }
         }
-        function init(_event) {
-            console.log("Hallo");
-            console.log(_event);
-            let h1 = document.getElementsByTagName("h1")[0];
-            h1.addEventListener("mouseover", handleMouseOverH1);
-            h1.addEventListener("click", handleClick);
+    }
+    function handleKeyDown(_event) {
+        if (_event.keyCode > 64 && _event.keyCode < 91 || _event.keyCode == 16 || _event.keyCode == 20) {
+            saveLetter = String.fromCharCode(_event.keyCode);
         }
-        function handleMouseOverH1(_event) {
-            console.log(_event.target);
-            let h1 = _event.target;
-            h1.style.color = "#ff0000";
+        else {
+            return;
         }
-    })(L08_Events || (L08_Events = {}));
+    }
+    function handleBoxClick(_event) {
+        if (saveLetter == "")
+            return;
+        text = document.createElement("div");
+        text.innerText = saveLetter;
+        text.style.position = "absolute";
+        text.style.color = "#000000";
+        text.style.width = "35px";
+        text.style.height = "35px";
+        text.style.fontSize = "30px";
+        text.style.left = _event.pageX + "px";
+        text.style.top = _event.pageY + "px";
+        text.addEventListener("click", deleteLetter);
+        document.body.appendChild(text);
+    }
+    function deleteLetter(_event) {
+        if (_event.altKey == false)
+            return;
+        else {
+            let remove = _event.target;
+            document.body.removeChild(remove);
+        }
+    }
 })(Aufgabe09 || (Aufgabe09 = {}));
 //# sourceMappingURL=Aufgabe-09-DynHTML.js.map
--- Aufgabe_09/Aufgabe-09-DynHTML.ts
+++ Aufgabe_09/Aufgabe-09-DynHTML.ts
@@ -1,42 +1,106 @@
 /*
-Aufgabe: (8 DynHTML)
+Aufgabe: (9 DynHTML)
 Name: (Julian Schüler)
 Matrikel: (256033)
-Datum: (07.12.2017)
+Datum: (15.12.2017)
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
 
 namespace Aufgabe09 {
+    
+    window.addEventListener("load", Erpresserbrief);
+    
+    window.addEventListener("keydown", handleKeyDown);
+    
+    let text: HTMLElement;
+    
+    let letters: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-    namespace L08_Events {
-        window.addEventListener("load", init);
-        document.addEventListener("keydown", handleKeydown);
-        //document.onkeydown = handleKeydown;
-        document.addEventListener("click", handleClick);
+    let saveLetter: string = "";
+   
+    function Erpresserbrief(): void {
+        
+        for (let i: number = 0; i < letters.length; i++) {  
+                 
+            let l: HTMLDivElement = document.createElement("div");
+            l.style.width = "35px";
+            l.style.height = "35px";
+            l.style.margin = "5px";
+            l.style.backgroundColor = "#b7b7b7";
+            l.style.color = "white";
+            l.style.fontSize = "25px";
+            l.innerText = letters[i];
+            l.id = letters[i];
+            l.className = "letters";
 
-        function handleClick(_event: MouseEvent): void {
-            console.log("Target: " + _event.target);
-            console.log("currentTarget: " + _event.currentTarget);
+            l.addEventListener("click", handleLetterClick);
+            document.body.appendChild(l);
         }
+            
+        let box: HTMLDivElement = document.createElement("div");        
+        box.style.width = "1165px";
+        box.style.height = "500px";
+        box.style.marginTop = "20px";
+        box.style.backgroundColor = "#b7b7b7";
+        
+        box.addEventListener("click", handleBoxClick);
+        document.body.appendChild(box);
+    }
 
-        function handleKeydown(_event: KeyboardEvent): void {
-            console.log(_event);
-        }
+    
+    function handleLetterClick(_event: MouseEvent): void {      
+        console.log(_event.target);
 
-        function init(_event: Event): void {
-            console.log("Hallo");
-            console.log(_event);
+        let l: HTMLDivElement = <HTMLDivElement>_event.target;
+        l.style.color = "#ffffff";
+        l.style.backgroundColor = "#000000";
+        saveLetter = l.id;
 
-            let h1: HTMLHeadingElement = document.getElementsByTagName("h1")[0];
-            h1.addEventListener("mouseover", handleMouseOverH1);
-            h1.addEventListener("click", handleClick);
-        }
+        let letterList: NodeListOf<HTMLDivElement> = <NodeListOf<HTMLDivElement>>document.getElementsByClassName("letters"); //Jedes Div der Klasse letters in Variable speichern
 
-        function handleMouseOverH1(_event: MouseEvent): void {
-            console.log(_event.target);
-            let h1: HTMLHeadingElement = <HTMLHeadingElement>_event.target;
-            h1.style.color = "#ff0000";
+        for (let i: number = 0; i < letterList.length; i++) {   // Wenn der gespeicherte Buchstabe nicht mit der ID aus der Buchstabenliste ¸bereinstimmt, wird die Farbe des Hintergrunds zur¸ckgesetzt 
+            if (saveLetter != letterList[i].id) {
+                letterList[i].style.backgroundColor = "#b7b7b7";
+            }
         }
     }
+        
+    function handleKeyDown(_event: KeyboardEvent): void {
+
+        if (_event.keyCode > 64 && _event.keyCode < 91 || _event.keyCode == 16 || _event.keyCode == 20) {
+            saveLetter = String.fromCharCode(_event.keyCode);
+
+        } else {
+            return;
+        }
+    }
+        
+    function handleBoxClick(_event: MouseEvent): void {
+        if (saveLetter == "")
+            return;
+
+        text = document.createElement("div");
+        text.innerText = saveLetter;
+        text.style.position = "absolute";
+        text.style.color = "#000000";
+        text.style.width = "35px";
+        text.style.height = "35px";
+        text.style.fontSize = "30px";
+        text.style.left = _event.pageX + "px";
+        text.style.top = _event.pageY + "px";
+        text.addEventListener("click", deleteLetter);
+        document.body.appendChild(text);
+    }
+
+    function deleteLetter(_event: MouseEvent): void {
+        if (_event.altKey == false)
+            return;
+
+        else {
+            let remove: HTMLDivElement = <HTMLDivElement>_event.target;
+            document.body.removeChild(remove);
+        }
+    }
+
 }
